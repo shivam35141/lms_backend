a3a3a209f52eebee297028a47df9c05c36486a76
Shivam Mehta <shivam35141@gmail.com>
2022-03-12 18:03:14 +0530

    - fixes

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -13,7 +13,7 @@ function authJwt() {
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
-            {url: /\/api\/v1\/users(.*)/,methods: ['GET']},
+            {url: /\/api\/v1\/users(.*)/,methods: ['GET','PUT']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]
